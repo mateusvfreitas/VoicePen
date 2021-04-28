2c7f59cb98b2db21628f549618466b4e5c1fc628
Gabriel Kuhnen <gabrielkuhnenb@gmail.com>
2021-04-27 23:02:43 -0300

    control

--- voicepen/app.py
+++ voicepen/app.py
@@ -1,8 +1,12 @@
 from .src import mic as m
 from .src import image as i
 from .src import lines as l
+from .src import control as c
 
 def run():
     text = m.start()
     path = i.set_image(text)
     l.main(path)
+
+    vp = c.VoicePen()
+    vp.draw_from_file("/home/pi/git/VoicePen/voicepen/text.json")
